e9734035d90107faa232175df31d0f2df8a3ee4c
Daniel Brenes <brenes.daniel@yahoo.com>
2020-08-26 20:54:40 -0500

    Playing around with debugging

---
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+ * Daniel Brenes
+ * ITSE 1430
+ * Lab 1 ( Include comments for Lab assignments only)
+ * Howdy Y'all
+ */
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            // A single line comment 
+            // Another line of comments
+
+            Console.WriteLine("Hello World!");
+
+            int hours = 40;
+            double payRate = 12.80;
+
+            double totalPay = hours * payRate;
+
+            //Another block
+            //10+45;
+
+        }
+    }
+}
